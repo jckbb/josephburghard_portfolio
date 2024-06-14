f500b909f45eeba129646b9cdbedb5f972486bd0
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-06-14 13:20:10 -0400

    update styling for mobile view

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import type { MetaFunction } from "@remix-run/node";
 import WebsiteIcon from "../assets/websiteicon.svg";
 import iOSIcon from "../assets/iosicon.svg";
@@ -45,18 +44,11 @@ const projectList = [
 ];
 
 export default function Index() {
-  const [isMobileState, setMobileState] = useState(false);
-
-  useEffect(() => {
-    const { innerWidth: width } = window;
-
-    setMobileState(width < 768);
-  }, []);
-
+  // === HERO ===
   const IntroSection = (
-    <div className="bg-[#F3F3F3] md:col-span-4 md:row-span-2 items-center flex flex-col overflow-hidden">
-      <div className="flex flex-col col-span-4 row-span-2 w-[50rem]">
-        <div className="px-[1rem] flex flex-col gap-[1rem]">
+    <div className="bg-[#F3F3F3] col-span-1 md:col-span-4 row-span-2 md:items-center md:flex md:flex-col md:overflow-hidden">
+      <div className="flex flex-col col-span-4 row-span-2 lg:w-[50rem] px-[1rem] pb-[1rem]">
+        <div className="flex flex-col gap-[1rem]">
           <div className="flex justify-center pt-[2rem]">
             <img
               className="rounded-full w-[30rem]"
@@ -68,22 +60,23 @@ export default function Index() {
             <span className="lg:absolute lg:right-[4.6rem] lg:top-[1.2rem] text-white text-[1.3rem] font-sans bg-[#0984E3] py-[1rem] px-[2rem] flex items-center justify-center rounded-full">
               Mobile/Frontend Developer
             </span>
-            <h1 className="text-[3rem] md:text-[3.5rem] lg:text-[5rem] leading-tight font-sans font-bold">
+            <h1 className="text-center md:text-start text-[3rem] md:text-[3.5rem] lg:text-[5rem] leading-tight font-sans font-bold">
               Hi! I Am
               <br />
               Joseph Burghard
             </h1>
-            <p className="text-center font-sans text-[1.2rem] leading-tight">
+            <span className=" text-center font-sans text-[1.2rem] leading-tight">
               Cross Platform Development Specialist with a focus on leveraging
               new technologies and implementations
-            </p>
+            </span>
           </div>
         </div>
       </div>
     </div>
   );
+  // === PROJECTS ===
   const ProjectsSection = (
-    <div className="md:col-span-3 md:row-span-3 overflow-y-scroll">
+    <div className="col-span-1 md:col-span-3 row-span-3 md:overflow-y-scroll">
       <div className="p-[3rem] w-full flex flex-col flex-1 gap-[2rem]">
         <h2 className="text-[2rem] font-sans font-bold">{"Projects"}</h2>
         {projectList.map((project, index) => (
@@ -92,22 +85,22 @@ export default function Index() {
             target="_blank"
             rel="noreferrer"
             key={index}
-            className="bg-[#222222] gap-[2rem] md:gap-[4rem] items-center px-[2rem] md:px-[3rem] rounded-full flex h-[8rem] md:h-[13rem] hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:shadow-lg"
+            className="bg-[#222222] gap-[1rem] items-center px-[2rem] lg:px-[3rem] rounded-full flex h-[8rem] lg:h-[13rem] hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:shadow-lg"
           >
             <div>
-              <div className="h-[4rem] w-[4rem] md:h-[8rem] md:w-[8rem] flex justify-center items-center rounded-full bg-white">
+              <div className="h-[4rem] w-[4rem] lg:h-[8rem] lg:w-[8rem] flex justify-center items-center rounded-full bg-white">
                 <img
                   alt="thumbnail"
                   src={project.platform === "ios_app" ? iOSIcon : WebsiteIcon}
-                  className="h-[3rem] w-[3rem] md:h-[5rem] md:w-[5rem]"
+                  className="h-[3rem] w-[3rem] lg:h-[5rem] lg:w-[5rem]"
                 />
               </div>
             </div>
-            <div className="flex flex-1 flex-col overflow-hidden">
-              <span className="text-white text-[1.5rem] md:text-[2rem] truncate">
+            <div className="flex flex-col overflow-hidden">
+              <span className="text-white text-[1.5rem] lg:text-[2rem] truncate">
                 {project.name}
               </span>
-              <span className="text-white text-[0.8rem] md:text-[1rem] truncate">
+              <span className="text-white text-[0.8rem] lg:text-[1rem] truncate">
                 {project.subtitle}
               </span>
             </div>
@@ -116,8 +109,9 @@ export default function Index() {
       </div>
     </div>
   );
+  // === CONTACT ===
   const ContactSection = (
-    <div className="bg-[#222222] md:col-span-4 md:row-span-1 px-[5rem] justify-center flex flex-col">
+    <div className="py-[3rem] bg-[#222222] md:col-span-4 md:row-span-1 px-[5rem] justify-center flex flex-col">
       <div className="grid grid-cols-2 gap-4">
         {infomationList.map((info, index) => (
           <div key={index} className="">
@@ -130,11 +124,10 @@ export default function Index() {
               <span className="font-sans font-bold text-white text-[1.3rem] capitalize">
                 {info.label}
               </span>
-              {!isMobileState && (
-                <span className="hover:underline decoration-white font-sans text-white text-[1.3rem] text-opacity-80 truncate">
-                  {info.link}
-                </span>
-              )}
+
+              <span className="invisible md:visible hover:underline decoration-white font-sans text-white text-[1.3rem] text-opacity-80 truncate">
+                {info.link}
+              </span>
             </a>
           </div>
         ))}
@@ -143,7 +136,7 @@ export default function Index() {
   );
 
   return (
-    <main className="h-screen w-screen md:grid md:grid-flow-row-dense md:grid-cols-7 md:grid-rows-3">
+    <main className="h-screen w-screen grid grid-cols-1 grid-flow-row md:grid-cols-7 md:grid-rows-3">
       {IntroSection}
       {ProjectsSection}
       {ContactSection}
