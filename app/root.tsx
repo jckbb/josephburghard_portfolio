d7a428703f18f203a87f2919b028f4c3037233fc
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 07:44:50 -0400

    update root with theme provider, styling and theme script to check default theme before hydration

--- app/root.tsx
+++ app/root.tsx
@@ -6,26 +6,44 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import clsx from "clsx";
 import stylesheet from "~/tailwind.css?url";
+import {
+  NonFlashOfWrongThemeEls,
+  ThemeProvider,
+  useTheme,
+} from "./utils/themeProvider";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-export default function App() {
+const App = () => {
+  const [theme] = useTheme();
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(theme)}>
       <head>
+        <NonFlashOfWrongThemeEls />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="md:fixed bg-[#FFEAA7]">
+      <body className="md:fixed">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
+};
+
+const AppWithProviders = () => {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+};
+
+export default AppWithProviders;
