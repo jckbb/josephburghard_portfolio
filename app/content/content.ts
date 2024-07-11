670285cc4b67cb0298464e3703ad68fb43ff2884
joseph burghard <josephburghard@josephs-MacBook-Pro.local>
2024-07-11 08:01:27 -0400

    add content file

---
+++ app/content/content.ts
@@ -0,0 +1,23 @@
+type Info = {
+  label: string;
+  link: string;
+  type: "mail" | "url";
+};
+
+export const infomationList: Info[] = [
+  {
+    label: "contact",
+    link: "josephburghard@gmail.com",
+    type: "mail",
+  },
+  {
+    label: "linkedin",
+    link: "https://www.linkedin.com/in/joecburghard/",
+    type: "url",
+  },
+  {
+    label: "github",
+    link: "https://github.com/jckbb",
+    type: "url",
+  },
+];
